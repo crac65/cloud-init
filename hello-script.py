4b25f962666479dbdf7bc39ea935b0c2aa6cf385
crac65 <68588504+crac65@users.noreply.github.com>
2024-03-01 11:37:34 +0000

    Create hello-script.py

---
+++ hello-script.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+print ("Hello form python")
+
+import pexpect
+import os
+
+password = "kM:Y_<QW-G~3L6fvcurepaword2023"
+
+(output, status) = pexpect.run(
+    "/opt/emc/boostfs/bin/boostfs lockbox set -u boost_caas -d 10.2.119.196 -s stu_caas",
+    withexitstatus=True,
+    encoding='utf8',
+    events={ "Enter storage unit user password": password + '\n' }
+)
+
+os.system('mkdir /mnt/testbackup')
+os.system('/opt/emc/boostfs/bin/boostfs  mount -d 10.2.119.196 -s stu_caas /mnt/testbackup')
+
+if status == 0:
+    print("it worked!")
+else:
+    print("it didn't work -- status code was ", status)
+    print(output)
